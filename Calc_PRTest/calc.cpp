689aff655586c40451471574c24d9e0291e3325d
tamongbo <tamongbo@gmail.com>
2024-05-20 16:40:18 +0900

    first commit

---
+++ Calc_PRTest/calc.cpp
@@ -0,0 +1,21 @@
+class Calc {
+public:
+	int getSum(int a, int b) {
+		return 0;
+	}
+	int getGop(int a, int b) {
+		return 0;
+	}
+	int getZegop(int a) {
+		return 0;
+	}
+	int getMinus(int a, int b) {
+		return 0;
+	}
+	int getDivide(int a, int b) {
+		return 0;
+	}
+	int getSumSum(int a, int b, int c) {
+		return 0;
+	}
+};
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Calc_PRTest/calc.cpp"
+
+TEST(TestCaseName, TestName) {
+	Calc calc;
+	EXPECT_EQ(1, 1);
+}
