df3ea0fbf89d5f00b660ce944dc7db70b053154b
sunghee.kim <sunghee3.kim@gmail.com>
2024-05-20 17:06:32 +0900

    bug fic

--- Calc_PRTest/calc.cpp
+++ Calc_PRTest/calc.cpp
@@ -13,14 +13,14 @@ public:
 		return 0;
 	}
 	int getMinus(int a, int b) {
+		return 0;
+	}
+	int getDivide(int a, int b) {
 		if (b == 0)
 		{
 			throw invalid_argument("It can't be zero");
 		}
 		return a / b;
-	}
-	int getDivide(int a, int b) {
-		return 0;
 	}
 	int getSumSum(int a, int b, int c) {
 		return 0;
